7b35112925e0e8ba84417e28bdabe991b955c8b4
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-05 14:23:36 +0530

    1.Two Sum

---
+++ LeetCode75/Solution.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode75.Helpers;
+
+namespace LeetCode75;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> indexes = new();
+
+        int i = 0;
+        while (i < nums.Length)
+        {
+            int findnum = target - nums[i];
+            if (indexes.ContainsKey(findnum))
+                return new int[] { indexes[findnum], i };
+            if (!indexes.ContainsKey(nums[i]))
+                indexes.Add(nums[i], i);
+            i++;
+        }
+        return new int[1];
+    }
+}
